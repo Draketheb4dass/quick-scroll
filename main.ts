235b976cb76da9493018d526fc57c678433be606
Jephte Colin <drakecolinj@gmail.com>
2024-10-10 17:16:46 -0400

    Add scroll down feature

--- main.ts
+++ main.ts
@@ -1,134 +1,145 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, View, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    mySetting: 'default'
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+let globalMarkdownView: MarkdownView | null = null;
 
-	async onload() {
-		await this.loadSettings();
+export default class HelloWorldPlugin extends Plugin {
+    settings: MyPluginSettings;
+    private scrollButton: HTMLButtonElement;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    async onload() {
+        console.log('loading plugin');
+        await this.loadSettings();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.scrollButton = this.createScrollButton();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // Add the button to the Obsidian editor container
+        document.body.appendChild(this.scrollButton);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        // Add an event listener to scroll down when the button is clicked
+        this.scrollButton.addEventListener("click", this.scrollToBottom.bind(this));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+        // Using this function will automatically remove the event listener when this plugin is disabled.
+        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+            console.log('click', evt);
+        });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    }
 
-	onunload() {
+    onunload() {
+        console.log('unloading plugin');
+        if (this.scrollButton) {
+            this.scrollButton.remove();
+        }
+    }
 
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+    createScrollButton() {
+        // Type assertion to specify that this is an HTMLButtonElement
+        this.scrollButton = document.createElement('button') as HTMLButtonElement;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+        this.scrollButton.innerText = '↓'; // Down arrow symbol
+        this.scrollButton.style.position = 'fixed';
+        this.scrollButton.style.bottom = '20px'; // Space from bottom
+        this.scrollButton.style.left = '50%'; // Center horizontally
+        this.scrollButton.style.transform = 'translateX(-50%)'; // Align center
+        this.scrollButton.style.zIndex = '1000';
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+        // Apply styles for position, size, background, and shadow
+        this.scrollButton.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)';
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+        // Circular shape
+        this.scrollButton.style.width = '30px';
+        this.scrollButton.style.height = '30px';
+        this.scrollButton.style.borderRadius = '40%';
+        this.scrollButton.style.backgroundColor = '#007BFF'; // Blue background
+        this.scrollButton.style.color = 'white';
+
+        return this.scrollButton;
+    }
+
+    private scrollToBottom = async () => {
+        const markdownView = this.getCurrentViewOfType();
+        if (markdownView) {
+            const file = this.app.workspace.getActiveFile();
+            const content = await (this.app as any).vault.cachedRead(file);
+            const lines = content.split('\n');
+            let numberOfLines = lines.length;
+
+            // In preview mode, don't count empty lines at the end
+            if (markdownView.getMode() === 'preview') {
+                while (numberOfLines > 0 && lines[numberOfLines - 1].trim() === '') {
+                    numberOfLines--;
+                }
+            }
+            markdownView.currentMode.applyScroll(numberOfLines - 1);
+        }
+    };
+
+    public getCurrentViewOfType() {
+        // Get the current active view
+        let markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+        // To distinguish whether the current view is hidden or not markdownView
+        const currentView = this.app.workspace.getActiveViewOfType(View) as MarkdownView;
+
+        // Solve the problem of closing always focus new tab setting
+        if (markdownView !== null) {
+            globalMarkdownView = markdownView;
+        } else {
+            // Fix the plugin shutdown problem when the current view is not exist
+            if (currentView == null || currentView?.file?.extension === "md") {
+                markdownView = globalMarkdownView;
+            }
+        }
+        return markdownView;
+    }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    plugin: HelloWorldPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: HelloWorldPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Setting #1')
+            .setDesc("It's a secret")
+            .addText(text => text
+                .setPlaceholder('Enter your secret')
+                .setValue(this.plugin.settings.mySetting)
+                .onChange(async (value) => {
+                    this.plugin.settings.mySetting = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
